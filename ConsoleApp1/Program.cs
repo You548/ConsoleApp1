765deac9b4b45dddcc6d0a24a5c650fb242a7b9b
NAOGAS LOAN <102701854+You548@users.noreply.github.com>
2022-08-03 06:31:27 +0900

    プロジェクト ファイルを追加します。

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        private const string Value = "Japanese";
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Value);
+        }
+    }
+}
